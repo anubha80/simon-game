44d461e7243b4b3fbaa52044423b1e42963d6f7c
anubha80 <85583058+anubha80@users.noreply.github.com>
2022-03-11 15:36:32 -0500

    Created animation and sound for user clicks

--- JS/game.js
+++ JS/game.js
@@ -1,39 +1,63 @@
 const colors=["green","red", "yellow","blue"];
 const randomNumberList=[];
+const gamePattern=[];
+const userPattern=[];
 var levelNumber=1;
+// generate random number between 1 to 4
 var randomNumber= (Math.floor(Math.random() * 4)+1);
 randomNumberList.push(randomNumber);
-console.log(randomNumber);
-detectGameStart();
-function detectGameStart(){
-    $(document).on('keypress',function(e) {
-        // checking if space key is pressed
-        if(e.which == 32) {
-            console.log('Space key pressed');
-            $(".headline").text("Level "+levelNumber);
-            gameStart();
-        }
-        // if any other key then error warning
-        else{
-            var audio = new Audio('./sounds/wrong.mp3');
-            audio.play();
-            document.querySelector("body").classList.add("wrong-key-pressed");
-            setTimeout(function(){
-                document.querySelector("body").classList.remove("wrong-key-pressed");
-            },300);
-        }
-    });
+var gameStarted=false;
+$(document).on('keypress',function(e) {
+    // checking if space key is pressed
+    if(e.which == 32) {
+        console.log('Space key pressed');
+        $(".headline").text("Level "+levelNumber);
+        nextPattern()
+        gameStarted=true;
+    }
+    // if any other key then error warning
+    else{
+        var audio = new Audio('./sounds/wrong.mp3');
+        audio.play();
+        document.querySelector("body").classList.add("wrong-key-pressed");
+        setTimeout(function(){
+            document.querySelector("body").classList.remove("wrong-key-pressed");
+        },300);
+    }
+});
+
+$(".btn").click(function(e){
+    var userChosenColor = e.target.id;
+    userPattern.push(userChosenColor);
+    console.log(userPattern);
+    playSound(userChosenColor);
+    checkPattern();
+});
+
+function checkPattern(){
+    
 }
 
-function gameStart(){
-    var audio = new Audio('./sounds/'+colors[randomNumber-1]+'.mp3');
+function nextPattern(){
+    randomNumber= (Math.floor(Math.random() * 4)+1);
+    console.log('Random number: '+randomNumber);
+    gamePattern.push(randomNumber);
+    playSound(colors[gamePattern[gamePattern.length-1]-1]);
+    console.log("game pattern: "+gamePattern);
+}
+
+// name parameter is always taking color as input
+function playSound(name){
+    console.log('playing --> ./sounds/'+name+'.mp3');
+    var audio = new Audio('./sounds/'+name+'.mp3');
     audio.play();
-    document.querySelector(".box"+randomNumber).classList.add("pressed");
+    document.querySelector("#"+name).classList.add("pressed");
     setTimeout(function(){
-        document.querySelector(".box"+randomNumber).classList.remove("pressed");
+        document.querySelector("#"+name).classList.remove("pressed");
     },300);
-    console.log(colors[randomNumber-1]+' box selected');
-    console.log('list of random numbers -> '+randomNumberList[0]);
-    console.log('length of random number array -> '+randomNumberList.length);
 }
-
+function startOver(){
+    levelNumber=1;
+    gamePattern=[];
+    gameStarted=false;
+}
