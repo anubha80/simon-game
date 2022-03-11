ce7fa749bd453b85ef2ea3ceb5aa381b493ab839
anubha80 <85583058+anubha80@users.noreply.github.com>
2022-03-11 16:55:42 -0500

    Modified game & user pattern functionality

--- JS/game.js
+++ JS/game.js
@@ -1,28 +1,24 @@
-const colors=["green","red", "yellow","blue"];
-const randomNumberList=[];
-const gamePattern=[];
-const userPattern=[];
-var levelNumber=1;
+var colors=["green","red", "yellow","blue"];
+var randomNumberList=[];
+var gamePattern=[];
+var userPattern=[];
+var levelNumber=0;
 // generate random number between 1 to 4
-var randomNumber= (Math.floor(Math.random() * 4)+1);
-randomNumberList.push(randomNumber);
 var gameStarted=false;
+
 $(document).on('keypress',function(e) {
     // checking if space key is pressed
-    if(e.which == 32) {
+    if(e.which == 32 && gameStarted==false) {
         console.log('Space key pressed');
-        $(".headline").text("Level "+levelNumber);
+        // $(".headline").text("Level "+levelNumber);
+        // $(".sub-heading").text("");
+        displayLevel(levelNumber);
         nextPattern()
         gameStarted=true;
     }
     // if any other key then error warning
     else{
-        var audio = new Audio('./sounds/wrong.mp3');
-        audio.play();
-        document.querySelector("body").classList.add("wrong-key-pressed");
-        setTimeout(function(){
-            document.querySelector("body").classList.remove("wrong-key-pressed");
-        },300);
+        playSound("wrong");
     }
 });
 
@@ -31,18 +27,32 @@ $(".btn").click(function(e){
     userPattern.push(userChosenColor);
     console.log(userPattern);
     playSound(userChosenColor);
-    checkPattern();
+    checkPattern(userPattern.length-1);
 });
 
-function checkPattern(){
-    
+function checkPattern(currentLevel){
+    if (userPattern[currentLevel]=== gamePattern[currentLevel]){
+        if(userPattern.length===gamePattern.length){
+            setTimeout(function(){
+                nextPattern();
+            },1000);
+        } 
+    }
+    else{
+        playSound("wrong"); 
+    }
 }
 
 function nextPattern(){
+    userPattern=[];
+    levelNumber++;
+    // $(".headline").text("Level "+levelNumber);
+    // $(".sub-heading").text("");
+    displayLevel(levelNumber);
     randomNumber= (Math.floor(Math.random() * 4)+1);
     console.log('Random number: '+randomNumber);
-    gamePattern.push(randomNumber);
-    playSound(colors[gamePattern[gamePattern.length-1]-1]);
+    gamePattern.push(colors[randomNumber-1]);
+    playSound(colors[randomNumber-1]);
     console.log("game pattern: "+gamePattern);
 }
 
@@ -51,13 +61,40 @@ function playSound(name){
     console.log('playing --> ./sounds/'+name+'.mp3');
     var audio = new Audio('./sounds/'+name+'.mp3');
     audio.play();
-    document.querySelector("#"+name).classList.add("pressed");
-    setTimeout(function(){
-        document.querySelector("#"+name).classList.remove("pressed");
-    },300);
+    if (name=="wrong"){
+        document.querySelector("body").classList.add("wrong-key-pressed");
+        setTimeout(function(){
+            document.querySelector("body").classList.remove("wrong-key-pressed");
+        },300);
+        $(".headline").text("Game Over 😵‍💫");
+        $(".sub-heading").text("Press space key to restart");
+        startOver();
+    }
+    else{
+        $("#" + name).addClass("pressed");
+        setTimeout(function () {
+            $("#" + name).removeClass("pressed");
+        }, 100);
+    }
+    
 }
 function startOver(){
-    levelNumber=1;
+    levelNumber=0;
     gamePattern=[];
     gameStarted=false;
+}
+function displayLevel(levelNumber){
+    if (levelNumber<=3){
+        $(".headline").text("🔥 Level "+levelNumber+" 🔥");
+    }
+    else if(levelNumber<=6){
+        $(".headline").text("🔥🔥 Level "+levelNumber+" 🔥🔥");
+    }
+    else if (levelNumber<=10){
+        $(".headline").text("🔥🔥🔥🔥 Level "+levelNumber+" 🔥🔥🔥🔥");
+    }
+    else{
+        $(".headline").text("Level "+levelNumber);
+    }
+    $(".sub-heading").text("");
 }
